4349341ee24f21e478491025969f957c81722dc2
Nuked <nuked8@gmail.com>
2023-03-26 22:13:30 +0200

    Removed end of text

--- server.cpp
+++ server.cpp
@@ -1086,7 +1086,7 @@ int main(int argc, char ** argv) {
                     // end of text token
                     if (embd.back() == 2) {
                 
-                            output += "\n[end of text]\n";
+                            // output += "\n[end of text]\n";
                             
                             break;
                         
